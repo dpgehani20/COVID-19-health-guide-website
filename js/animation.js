18aeffb0c449b3ddc06154678409accf3ffb29b9
dpgehani20 <69641659+dpgehani20@users.noreply.github.com>
2021-08-03 19:58:47 +0530

    create js file

---
+++ js/animation.js
@@ -0,0 +1,7 @@
+let menu = document.querySelector('#menu-bar');
+let nav = document.querySelector('.nav');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    nav.classList.toggle('active');
+}
